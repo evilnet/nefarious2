5958dca2bc5eb49ffde98b6cb0f3d56791aeda8a
Matthew Beeching <jobe@mdbnet.co.uk>
2012-11-01 18:19:55 +0000

    Forgot to add support for the multi-prefix CAP in the WHO command output

--- ircd/whocmds.c
+++ ircd/whocmds.c
@@ -172,7 +172,7 @@ void do_who(struct Client* sptr, struct Client* acptr, struct Channel* repchan,
     if (!chan) {
       /* No flags possible for the channel, so skip them all. */
     }
-    else if (fields) {
+    else if ((IsNamesX(sptr) || CapActive(sptr, CAP_NAMESX)) || fields) {
       /* If you specified flags then we assume you know how to parse
        * multiple channel status flags, as this is currently the only
        * way to know if someone has @'s *and* is +'d.
