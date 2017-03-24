fecf2ac35d59c4093e39013177882787def0a0bf
Matthew Beeching <jobe@mdbnet.co.uk>
2017-03-24 23:33:27 +0000

    Added SSL library version as well as header version to VERSION output

--- ircd/m_version.c
+++ ircd/m_version.c
@@ -151,7 +151,8 @@ int mo_version(struct Client* cptr, struct Client* sptr, int parc, char* parv[])
     send_reply(sptr, RPL_VERSION, version, cvs_version, debugmode, cli_name(&me),
 	       debug_serveropts());
 #ifdef USE_SSL
-    sendcmdto_one(&me, CMD_NOTICE, sptr, "%C :%s", sptr, OPENSSL_VERSION_TEXT);
+    sendcmdto_one(&me, CMD_NOTICE, sptr, "%C :Headers: %s", sptr, OPENSSL_VERSION_TEXT);
+    sendcmdto_one(&me, CMD_NOTICE, sptr, "%C :Library: %s", sptr, SSLeay_version(SSLEAY_VERSION));
 #endif
 #ifdef USE_GEOIP
     sendcmdto_one(&me, CMD_NOTICE, sptr, "%C :GeoIP %s", sptr, geoip_version());
