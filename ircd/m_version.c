fc8257ab85d66d35c36c26a0ffa87b752c10694a
Matthew Beeching <jobe@mdbnet.co.uk>
2017-04-15 23:08:48 +0100

    Only show OpenSSL header version if compiled with debug mode

--- ircd/m_version.c
+++ ircd/m_version.c
@@ -151,7 +151,9 @@ int mo_version(struct Client* cptr, struct Client* sptr, int parc, char* parv[])
     send_reply(sptr, RPL_VERSION, version, cvs_version, debugmode, cli_name(&me),
 	       debug_serveropts());
 #ifdef USE_SSL
+#ifdef DEBUGMODE
     sendcmdto_one(&me, CMD_NOTICE, sptr, "%C :Headers: %s", sptr, OPENSSL_VERSION_TEXT);
+#endif
     sendcmdto_one(&me, CMD_NOTICE, sptr, "%C :Library: %s", sptr, SSLeay_version(SSLEAY_VERSION));
 #endif
 #ifdef USE_GEOIP
