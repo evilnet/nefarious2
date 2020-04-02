492a0d184d58e80ebd885e21e0ecf2d2b5f77250
Alex Schumann <rubin@afternet.org>
2020-04-02 12:09:52 -0700

    change to the correct signal for reloading ssl certs

--- tools/linesync/gitsync.sh
+++ tools/linesync/gitsync.sh
@@ -279,7 +279,7 @@ if [ ! -z "$diff" ]; then
 	cp "$tmp_path/linesync.new.$TS" "$cpath"
 
 	# Rehash ircd (without caring wether or not it succeeds)
-	kill -HUP `cat "$ppath" 2>/dev/null` > /dev/null 2>&1
+	kill -USR1 `cat "$ppath" 2>/dev/null` > /dev/null 2>&1
 
     #todo: kill iauthd?
 fi
